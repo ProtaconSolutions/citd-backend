0a4068914fc36b215c51c4d6e919b83cd8b8e2c4
Savpek <savolainen.pekka@gmail.com>
2016-11-04 13:28:11 +0200

    Implemented signalr hub for angular 2.

---
+++ citd-backend/Hubs/ChannelEvent.cs
@@ -0,0 +1,29 @@
+using System;
+using Newtonsoft.Json;
+
+public class ChannelEvent
+{
+    public string Name { get; set; }
+
+    public string ChannelName { get; set; }
+
+    public DateTimeOffset Timestamp { get; set; }
+
+    public object Data
+    {
+        get { return _data; }
+        set
+        {
+            _data = value;
+            this.Json = JsonConvert.SerializeObject(_data);
+        }
+    }
+    private object _data;
+
+    public string Json { get; private set; }
+
+    public ChannelEvent()
+    {
+        Timestamp = DateTimeOffset.Now;
+    }
+}
